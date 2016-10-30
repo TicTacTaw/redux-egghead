e4642af7158ab98c52267bd6223630c4f396c1db
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-10-30 14:52:51 +0100

    React Todo List Example (Adding a Todo)

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './components/Menu.jsx';
+import { combineReducers, createStore } from 'redux';
 
-ReactDOM.render(
-  <Menu />,
-  document.getElementById('app')
-);
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+
+      return _extends({}, state, {
+        completed: !state.completed
+      });
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t =>
+        todo(t, action)
+      );
+    default:
+      return state;
+  }
+};
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+const store = createStore(todoApp);
+
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+  render () {
+    return (
+      <div>
+        <input ref={ node => {
+            this.input = node;
+          }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          this.input.value = '';
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          { this.props.todos.map(todo =>
+            <li key={todo.id}>
+              { todo.text }
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp
+      todos={ store.getState().todos }
+    />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
