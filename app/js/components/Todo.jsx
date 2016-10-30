db84e9722aff2b387a0c14bd60e0444c4910dcd0
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-10-30 20:47:29 +0100

    Structuring files and folders

---
+++ app/js/components/Todo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      cursor: 'pointer',
+      textDecoration: completed ? 'line-through' : 'none',
+    }}
+  >
+    {text}
+  </li>
+);
+
+Todo.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+  completed: React.PropTypes.bool.isRequired,
+  text: React.PropTypes.string.isRequired,
+};
+
+export default Todo;
---
+++ app/js/reducers/todos.js
@@ -0,0 +1,39 @@
+let _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false,
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return _extends({}, state, {
+        completed: !state.completed
+      });
+    default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action),
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t =>
+        todo(t, action)
+      );
+    default:
+      return state;
+  }
+};
+
+export default todos;
