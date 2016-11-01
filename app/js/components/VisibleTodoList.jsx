c3dbfad55562742a5582ff6fb87fcc8061373ef7
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-11-01 12:28:58 +0100

    Dispatching Actions with the Fetched Data

--- app/js/components/VisibleTodoList.jsx
+++ app/js/components/VisibleTodoList.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { toggleTodo } from '../actions';
+import * as actions from '../actions';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
 import { fetchTodos } from '../api';
 
 class VisibleTodoList extends React.Component {
   componentDidMount() {
-    fetchTodos(this.props.filter).then(todos =>
-      console.log(this.props.filter, todos)
-    );
+    this.fetchData();
   }
 
   componentDidUpdate(prevProps, prevState) {
       if (this.props.filter !== prevProps.filter) {
-        fetchTodos(this.props.filter).then(todos =>
-          console.log(this.props.filter, todos)
-        );
+        this.fetchData();
       }
   }
 
+  fetchData() {
+    const { filter, receiveTodos } = this.props;
+    fetchTodos(filter).then(todos =>
+      receiveTodos(filter, todos)
+    );
+  }
   render() {
-    return <TodoList {...this.props} />
+    const { toggleTodo, ...rest } = this.props;
+    return (
+      <TodoList
+        {...rest}
+        onTodoClick={toggleTodo}
+      />
+    );
   }
 }
 
@@ -44,7 +52,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  { onTodoClick: toggleTodo }
+  actions
 )(VisibleTodoList));
 
 export default VisibleTodoList;
